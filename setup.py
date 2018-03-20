3a6b554dd165f25f25ddd2a69ebcd890bd60da1f
Steven Callister <32499763+scallister@users.noreply.github.com>
2018-03-20 16:52:49 -0700

    Bumping version to 1.1.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ except IOError:
 
 setup(
     name='fossor',
-    version='1.0.0',
+    version='1.1.0',
     description=description,
     long_description=description,
     url='https://github.com/linkedin/fossor',
